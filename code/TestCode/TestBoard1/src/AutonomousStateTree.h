63a205828d033e5bf66a9cd9b8c26008c525385e
Aaron Cohen <throughthefire@derpymail.org>
2015-01-12 17:21:25 -0800

    Added control class for Autonomous mode

---
+++ code/TestCode/TestBoard1/src/AutonomousStateTree.h
@@ -0,0 +1,17 @@
+/*
+ * AutonomousStateTree.h
+ *
+ *  Created on: Jan 12, 2015
+ *      Author: acohen
+ */
+
+#ifndef SRC_AUTONOMOUSSTATETREE_H_
+#define SRC_AUTONOMOUSSTATETREE_H_
+
+class AutonomousStateTree {
+public:
+	AutonomousStateTree();
+	virtual ~AutonomousStateTree();
+};
+
+#endif /* SRC_AUTONOMOUSSTATETREE_H_ */
