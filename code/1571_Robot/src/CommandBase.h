3f7b4b612ff5cc65da90f6a94d8b6c58e0d31e68
Aaron Cohen <throughthefire@derpymail.org>
2015-02-02 18:22:32 -0800

    Added EncoderMotorSubsystem and SetEncoderMotorPositionCommand

--- code/1571_Robot/src/CommandBase.h
+++ code/1571_Robot/src/CommandBase.h
@@ -3,7 +3,7 @@
 
 #include <string>
 #include "Commands/Command.h"
-#include "Subsystems/ExampleSubsystem.h"
+#include "Subsystems/EncoderMotorSubsystem.h"
 #include "OI.h"
 #include "WPILib.h"
 
@@ -19,8 +19,8 @@ public:
 	CommandBase();
 	static void init();
 	// Create a single static instance of all of your subsystems
-	static ExampleSubsystem *examplesubsystem;
-	static OI *oi;
+	static EncoderMotorSubsystem* encodermotorsubsystem;
+	static OI* oi;
 };
 
 #endif
